ce06665144b550069f057eb482c291ec7c631417
Nicolás <ncomerci@itba.edu.ar>
2020-11-06 11:08:42 -0300

    boceto inicial del protocolo

---
+++ src/logger.c
@@ -0,0 +1,12 @@
+#include "../includes/logger.h"
+
+
+
+struct logger {
+    size_t historical_conections;   // una por usuario
+    size_t concurrent_conections;   // una por usuario
+    size_t total_bytes_transfered;  // aumenta cada vez que se hace un send con la cantidad enviada
+}
+
+
+// config en runtime -> cambiar tamaño de buffers
