411f878fd639f38d875409dc6e7f69f9a546046b
Nicolás <ncomerci@itba.edu.ar>
2020-11-05 23:06:06 -0300

    ipv4 socket replaced for ipv6

--- src/main.c
+++ src/main.c
@@ -52,83 +52,87 @@ int main(const int argc, char **argv)
     fd_selector selector = NULL;
 
     ///////////////////////////////////////////////////////////// IPv4
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(args.socks_port);
+    // struct sockaddr_in addr;
+    // memset(&addr, 0, sizeof(addr));
+    // addr.sin_family = AF_INET;
+    // addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    // addr.sin_port = htons(args.socks_port);
 
-    const int server4_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (server4_fd < 0)
-    {
-        err_msg = "unable to create ipv4 socket";
-        goto finally;
-    }
+    // const int server4_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // if (server4_fd < 0)
+    // {
+    //     err_msg = "unable to create ipv4 socket";
+    //     goto finally;
+    // }
 
-    fprintf(stdout, "Listening on ipv4 TCP port %d\n", args.socks_port);
+    // fprintf(stdout, "Listening on ipv4 TCP port %d\n", args.socks_port);
 
-    // man 7 ip. no importa reportar nada si falla.
-    setsockopt(server4_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+    // // man 7 ip. no importa reportar nada si falla.
+    // setsockopt(server4_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
 
-    if (bind(server4_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-        err_msg = "unable to bind ipv4 socket";
-        goto finally;
-    }
-    //TODO: change MAX PENDING CONNECTIONS
-    if (listen(server4_fd, 20) < 0)
-    {
-        err_msg = "unable to listen on ipv4 socket";
-        goto finally;
-    }
+    // if (bind(server4_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    // {
+    //     err_msg = "unable to bind ipv4 socket";
+    //     goto finally;
+    // }
+    // //TODO: change MAX PENDING CONNECTIONS
+    // if (listen(server4_fd, 20) < 0)
+    // {
+    //     err_msg = "unable to listen on ipv4 socket";
+    //     goto finally;
+    // }
 
     // registrar sigterm es útil para terminar el programa normalmente.
     // esto ayuda mucho en herramientas como valgrind.
     signal(SIGTERM, sigterm_handler);
     signal(SIGINT, sigterm_handler);
 
-    if (selector_fd_set_nio(server4_fd) == -1)
-    {
-        err_msg = "getting server ipv4 socket flags";
-        goto finally;
-    }
-
-    ///////////////////////////////////////////////////////////// IPv6
-    // TODO: create IPv6 socket
-    // struct sockaddr_in6 addr6;
-    // memset(&addr6, 0, sizeof(addr6));
-    // addr6.sin6_family = AF_INET6;
-    // addr6.sin6_port = htons(args.socks_port);
-    // addr6.sin6_addr = in6addr_any;
-    // const int server6_fd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
-    // if (server6_fd < 0)
-    // {
-    //     err_msg = "unable to create ipv6 socket";
-    //     goto finally;
-    // }
-
-    // fprintf(stdout, "Listening on ipv6 TCP port %d\n", args.socks_port);
-    // fprintf(stdout, "Listening on ipv6 fd %d\n", server6_fd);
-
-    // setsockopt(server6_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
-
-    // if (bind(server6_fd, (struct sockaddr *)&addr6,sizeof(addr6)) < 0)
-    // {
-    //     err_msg = "unable to bind ipv6 socket";
-    //     goto finally;
-    // }
-
-    // if (listen(server6_fd, 20) < 0)
-    // {
-    //     err_msg = "unable to listen on ipv6 socket";
-    //     goto finally;
-    // }
-
-    // if (selector_fd_set_nio(server6_fd) == -1)
+    // if (selector_fd_set_nio(server4_fd) == -1)
     // {
     //     err_msg = "getting server ipv4 socket flags";
     //     goto finally;
     // }
+
+    ///////////////////////////////////////////////////////////// IPv6
+    // TODO: create IPv6 socket
+    struct sockaddr_in6 addr6;
+    memset(&addr6, 0, sizeof(addr6));
+    addr6.sin6_family = AF_INET6;
+    addr6.sin6_addr = in6addr_any;
+    addr6.sin6_port = htons(args.socks_port);
+    
+    const int server6_fd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+    if (server6_fd < 0)
+    {
+        err_msg = "unable to create ipv6 socket";
+        goto finally;
+    }
+
+    fprintf(stdout, "Listening on ipv6 TCP port %d\n", args.socks_port);
+    // fprintf(stdout, "Listening on ipv6 fd %d\n", server6_fd);
+
+    if (setsockopt(server6_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0 ) {
+        err_msg = "setsockopt(SO_REUSEADDR) failed";
+        goto finally;
+    } 
+
+    if (bind(server6_fd, (struct sockaddr *)&addr6,sizeof(addr6)) < 0)
+    {
+        err_msg = "unable to bind ipv6 socket";
+        goto finally;
+    }
+
+    if (listen(server6_fd, 20) < 0)
+    {
+        err_msg = "unable to listen on ipv6 socket";
+        goto finally;
+    }
+
+    if (selector_fd_set_nio(server6_fd) == -1)
+    {
+        err_msg = "getting server ipv4 socket flags";
+        goto finally;
+    }
 
     const struct selector_init conf = {
         .signal = SIGALRM,
@@ -155,8 +159,8 @@ int main(const int argc, char **argv)
         .handle_close = NULL, // nada que liberar
     };
     // registering ipv4 passive socket
-    ss = selector_register(selector, server4_fd, &socksv5,
-                           OP_READ, NULL);
+    // ss = selector_register(selector, server4_fd, &socksv5,
+    //                        OP_READ, NULL);
 
     if (ss != SELECTOR_SUCCESS)
     {
@@ -164,8 +168,8 @@ int main(const int argc, char **argv)
         goto finally;
     }
     // registering ipv6 passive socket
-    // ss = selector_register(selector, server6_fd, &socksv5,
-    //                        OP_READ, NULL);
+    ss = selector_register(selector, server6_fd, &socksv5,
+                           OP_READ, NULL);
 
     if (ss != SELECTOR_SUCCESS)
     {
@@ -210,14 +214,14 @@ finally:
 
     socksv5_pool_destroy();
 
-    if (server4_fd >= 0)
-    {
-        close(server4_fd);
-    }
-
-    // if (server6_fd >= 0)
+    // if (server4_fd >= 0)
     // {
-    //     close(server6_fd);
+    //     close(server4_fd);
     // }
+
+    if (server6_fd >= 0)
+    {
+        close(server6_fd);
+    }
     return ret;
 }
